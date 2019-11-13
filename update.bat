e748ddf5de2bc56d9b995e6b972dd34c6d79bca8
Maxim Kulyk <mxx888777@gmail.com>
2019-11-13 16:43:23 +0200

    [batch] Rename

--- update.bat
+++ update.bat
@@ -1,6 +1,6 @@
 @echo off
 cls
 del yuzu-keys-installer.bat
-powershell.exe (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/hipeopeo/s/master/yuzu-keys-installer.bat', 'yuzu-keys-installer.bat')
+powershell.exe (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/zeewanderer/s/master/yuzu-tool.bat', 'yuzu-tool.bat')
 yuzu-keys-installer.bat
 exit
